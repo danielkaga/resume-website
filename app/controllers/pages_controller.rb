a6ccdfee43e09daa12f16ff3c5481e4ffb15b23a
Daniel Kaga <daniel.kaga@gmail.com>
2018-05-16 00:05:29 +1000

    Added skills to home page'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,2 @@
 class PagesController < ApplicationController
-  def home
-    @skills = []
-    @interests = ["surfboard.png", "film.png", "headphone-symbol.png", "sushi.png", "vinyl.png", "hockey.png"]
-  end
 end
