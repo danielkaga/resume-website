8af9513bdc848dba6098b87e88bafe027e782c6e
Daniel Kaga <daniel.kaga@gmail.com>
2018-05-15 23:25:23 +1000

    Added profile to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,6 @@
 class PagesController < ApplicationController
+  def home
+    @skills = []
+    @interests = ["surfboard.png", "film.png", "headphone-symbol.png", "sushi.png", "vinyl.png", "hockey.png"]
+  end
 end
