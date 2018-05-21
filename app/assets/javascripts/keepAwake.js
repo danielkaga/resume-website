ec5845ae2d4b1f129d851a66a44f60124e13fc3a
Daniel Kaga <daniel.kaga@gmail.com>
2018-05-21 14:19:31 +1000

    Added keepAwake js and css to all components

---
+++ app/assets/javascripts/keepAwake.js
@@ -0,0 +1,4 @@
+const http = require("http");
+setInterval(function() {
+    http.get("http://danielkaga.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
--- app/assets/javascripts/navBarScroll.js
+++ app/assets/javascripts/navBarScroll.js
@@ -1,10 +1,11 @@
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
+  let navbar = document.getElementById("navbar")
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    navbar.style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-80px";
+    navbar.style.top = "-80px";
   }
   if (currentScrollPos > (window.innerHeight - 20)) {
     document.querySelectorAll(".navbar-link").forEach(function(link) {
@@ -14,6 +15,13 @@ window.onscroll = function() {
     document.querySelectorAll(".navbar-link").forEach(function(link) {
       link.style.color = "white";
     });
+  }
+  if (currentScrollPos == 0) {
+    navbar.style.background = "rgba(255,255,255,0)";
+    navbar.style.boxShadow = "0 0px 0px rgba(0,0,0,0)";
+  } else {
+    navbar.style.background = "rgba(255,255,255,0.6)";
+    navbar.style.boxShadow = "0 2px 4px rgba(0,0,0,0.3)";
   }
   prevScrollpos = currentScrollPos;
 }
